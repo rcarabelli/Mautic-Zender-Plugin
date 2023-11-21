a0e5b889bc370b1ac69c7dbb366e7e13b3241ec7
Renato Carabelli <rcarabelli@gmail.com>
2023-11-20 23:27:26 -0500

    Enhanced webhook to tag leads in Mautic based on WhatsApp message responses

--- Controller/ZenderWebhookController.php
+++ Controller/ZenderWebhookController.php
@@ -39,9 +39,45 @@ class ZenderWebhookController extends CommonApiController
             return new JsonResponse(['message' => 'Unauthorized'], JsonResponse::HTTP_UNAUTHORIZED);
         }
     
+        // Assuming you have a way to get the LeadModel instance
+        $leadModel = $this->getModel('lead');
+        // Find lead by phone number
+        $lead = $leadModel->getRepository()->findOneBy(['mobile' => $phone]);
+    
+        if ($lead) {
+            // Get the tag repository
+            $tagRepository = $this->getModel('lead.tag')->getRepository();
+    
+            // Find or create the tag
+            $tagEntity = $tagRepository->findOneBy(['tag' => 'whatsapp message answered through zender']);
+            if (!$tagEntity) {
+                $tagEntity = new \Mautic\LeadBundle\Entity\Tag();
+                $tagEntity->setTag('whatsapp message answered through zender');
+                $tagRepository->saveEntity($tagEntity);
+            }
+    
+            // Add tag to lead
+            if (!$lead->getTags()->contains($tagEntity)) {
+                $lead->addTag($tagEntity);
+                $leadModel->saveEntity($lead);
+                $this->logger->debug('Tag added to lead.', [
+                    'leadId' => $lead->getId(),
+                    'tag' => $tagEntity->getTag()
+                ]);
+            } else {
+                $this->logger->debug('Lead already has the tag.', [
+                    'leadId' => $lead->getId(),
+                    'tag' => $tagEntity->getTag()
+                ]);
+            }
+        } else {
+            $this->logger->debug('Lead not found.', ['phone' => $phone]);
+        }
+    
         $this->logger->debug('Key validation successful.', [
             'received_key' => $key
         ]);
+    
         return new JsonResponse(['message' => 'Webhook received and key validated.'], JsonResponse::HTTP_OK);
     }
 
